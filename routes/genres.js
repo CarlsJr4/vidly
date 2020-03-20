b01de8d43bb00cbd6ac0da06d8d7fcc0c99db6ae
Carl Dungca <carl.dungca@yahoo.com>
2020-03-20 00:04:40 -0700

    Refactored app to properly handle errors using try catch

--- routes/genres.js
+++ routes/genres.js
@@ -28,28 +28,35 @@ const Genres = mongoose.model('Genre', new mongoose.Schema({
 // Default GET route
 router.get('/', async (req, res) => {
 	const genres = await Genres.find().sort('name');
-		res.send(genres);
+	res.send(genres);
 	}
 );
 
 // GET specific genre by ID
 router.get('/:id', async (req, res) => {
 	const id = req.params.id;
-	const genres = await Genres.find({_id: id});
-
-	if (!genres) return res.status(404).send('The genre with the given ID was not found.');
-
-	res.send(genres);
+	try {
+		const genres = await Genres.find({_id: id});
+		res.send(genres);
+	}
+	catch {
+		res.status(404).send('The genre with the given ID was not found.')
+	}
 });
 
 
 // POST request
 router.post('/', async (req, res) => {
 	// This returns the actual document. We can call the variable to save to DB.
-	let newGenre = new Genres({title: req.body.title});
-	// This returns a fulfilled promise so we can send it to the client
-	newGenre = await newGenre.save();
-	res.send(newGenre);
+	try {
+		let newGenre = new Genres({title: req.body.title});
+		// This returns a fulfilled promise so we can send it to the client
+		newGenre = await newGenre.save();
+		res.send(newGenre);
+	}
+	catch (err) {
+		res.send(err.errors.title.message);
+	}
 });
 
 
@@ -73,13 +80,15 @@ router.put('/:id', async (req, res) => {
 		
 
 // DELETE request
-router.delete('/:id', async(req, res) => {
-	const id = req.params.id;
-	const genre = await Genres.findByIdAndRemove(id);
-
-	if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-
-	res.send(genre);
+router.delete('/:id', async (req, res) => {
+	try {
+		const id = req.params.id;
+		const genre = await Genres.findByIdAndRemove(id);
+		res.send(genre);
+	}
+	catch {
+		res.status(404).send('The genre with the given ID was not found.');
+	}
 });
 
 module.exports = router;
