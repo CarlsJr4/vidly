def56d2a014d7407685bb757e6406738475ff0b3
Carl Dungca <carl.dungca@yahoo.com>
2020-03-20 14:55:58 -0700

    Wrote the rest of the CRUD routes for customers

--- routes/customers.js
+++ routes/customers.js
@@ -1,23 +1,21 @@
 // Build the customers API
-// What we need:
 const express = require('express');
 const mongoose = require('mongoose');
-// const Joi = require('@hapi/joi');
+const Joi = require('@hapi/joi');
 
 const router = express.Router();
 
 // Schema and model
-// We need database validation!
 const Customers = mongoose.model('Customer', 
 	new mongoose.Schema({
 		isGold: {
 			type: Boolean,
-			required: true
+			default: false
 		},
 		name: {
 			type: String,
 			minlength: 2,
-			maxlength: 20,
+			maxlength: 50,
 			required: true
 		}, 
 		phone: {
@@ -27,40 +25,88 @@ const Customers = mongoose.model('Customer',
 	})
 );
 
+// PUT and POST validation
+function validateData(customer) {
+	const schema = Joi.object({
+		name: Joi.string()
+			.min(2)
+			.max(50)
+			.required(),
+		phone: Joi.string()
+			.required(),
+		isGold: Joi.boolean()
+	});
+
+	return schema.validate(customer.body);
+}
+
 //  Route: vidly/api/customers. Need to build the routes for this.
-router.get('/', (req, res) => {
-	res.send('Here are vidlys customers!.');
+router.get('/', async (req, res) => {
+	const customers = await Customers.find();
+	res.send(customers);
 });
 
 //  Search for customer by ID
-router.get('/:id', (req, res) => {
-	res.send(`This is customer: ${req.params.id}`);
+router.get('/:id', async (req, res) => {
+	const id = req.params.id;
+	try {
+		const customer = await Customers.find({_id: id});
+		res.send(customer);
+	}
+	catch {
+		res.send(`Could not find customer with id ${id}`)
+	}
 });
 
 // POST request
 router.post('/', async (req, res) => {
-	// To save to DB we need a new instance of the model
-	try {
-		const customer = new Customers({
-			name: req.body.name, 
-			phone: req.body.phone,
-			isGold: req.body.isGold
-		});
-		const savedCustomer = await customer.save();
-		res.send(savedCustomer);
-	} 
-	catch (err) {
-		res.send(err);
-	}
+	// Here, we are letting Joi handle our errors
+	const { error } = validateData(req);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	const customer = new Customers({
+		name: req.body.name, 
+		phone: req.body.phone,
+		isGold: req.body.isGold
+	});
+
+	const savedCustomer = await customer.save();
+	res.send(savedCustomer);
 });
 
 // PUT request
+router.put('/:id', async (req, res) => {
+	const id = req.params.id;
+	const { error } = validateData(req);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	try {
+		// Returns a query object
+		let updatedCustomer = await Customers.findByIdAndUpdate(id, {
+			// Will this only update the fields we want to update?
+			// We could use the || operator incase the field doesn't exist?
+			name: req.body.name,
+			phone: req.body.phone,
+			isGold: req.body.isGold
+		},
+		{new: true});
+		res.send(updatedCustomer);
+	}
+	catch (err) {
+		res.send(err)
+	}
+});
 
 // DELETE request 
+router.delete('/:id', async (req, res) => {
+	try {
+		const id = req.params.id;
+		const customer = await Customers.findByIdAndRemove(id);
+		res.send(customer);
+	}
+	catch {
+		res.status(404).send('The customer with the given ID was not found.');
+	}
+});
 
-// Schema, model, class 
-// CRUD operations
-// Validation
-
-// NOTE: The name doesn't matter because we import the whole module anyways
 module.exports = router;
