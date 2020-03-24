51257cf605fe548fab7b8d1a07b028d69524eae0
Carl Dungca <carl.dungca@yahoo.com>
2020-03-23 23:01:07 -0700

    Added movies.js with project prompt

---
+++ models/movies.js
@@ -0,0 +1,11 @@
+// Embedded documents exercise
+// Movies schema should look like this:
+// _id
+// title: string
+// genre: embedded object with _id and name
+// numberInStock: number
+// dailyRentalRate: number
+
+// Task: Build an API to manage the list of movies
+// Do all the CRUD stuff
+// Feel free to copy/paste from your other files 
