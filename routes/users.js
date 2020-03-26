6f1f2973d6c8535f32025b81caacaae7056482b1
Carl Dungca <carl.dungca@yahoo.com>
2020-03-25 19:26:23 -0700

    Fixed a typo in users model import

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const users = require('../models/users');
+const {users} = require('../models/users');
 
 router.post('/', async (req, res) => {
 	const newUser = new users({
