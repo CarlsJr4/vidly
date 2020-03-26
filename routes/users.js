29a60cda04c3f8ceca6a67da00c60bd578901e60
Carl Dungca <carl.dungca@yahoo.com>
2020-03-25 19:22:21 -0700

    Added try catch block to post request

--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,19 @@ const router = express.Router();
 const users = require('../models/users');
 
 router.post('/', async (req, res) => {
-	// POST a new user to the database
 	const newUser = new users({
 		name: req.body.name,
 		email: req.body.email,
 		password: req.body.password
 	});
 
-	await newUser.save();
+	try {
+		await newUser.save();
+	} 
+	catch(err) {
+		return res.send(err);
+	}
+	
 	res.send(newUser);
 })
 
