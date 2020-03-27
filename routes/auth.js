c8330f76e0e64b87975e89194da648b93e92abcf
Carl Dungca <carl.dungca@yahoo.com>
2020-03-26 19:47:32 -0700

    Bugfix: fixed typo in decrypt method in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -14,7 +14,7 @@ router.post('/', async (req, res) => {
 	let user = await users.findOne(_.pick(req.body, ['email']));
 	if (!user) return res.status(400).send('Invalid email or password.');
 
-	const validPassword = await bcrytpt.compare(req.body.password, user.password);
+	const validPassword = await bcrypt.compare(req.body.password, user.password);
 	if (!validPassword) return res.status(400).send('Invalid email or password');
 
 	res.send(true);
