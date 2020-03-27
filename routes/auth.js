7a69326153d43b2a1b2ff204cd277721530abbee
Carl Dungca <carl.dungca@yahoo.com>
2020-03-26 19:43:14 -0700

    Wrote an auth POST route

---
+++ routes/auth.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const {users} = require('../models/users');
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+
+router.post('/', async (req, res) => {
+	// Validate the submitted user object
+	const {error} = validate(req);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	// Determine if user exists
+	let user = await users.findOne(_.pick(req.body, ['email']));
+	if (!user) return res.status(400).send('Invalid email or password.');
+
+	const validPassword = await bcrytpt.compare(req.body.password, user.password);
+	if (!validPassword) return res.status(400).send('Invalid email or password');
+
+	res.send(true);
+});
+
+function validate(req) {
+	const schema = Joi.object({
+		email: Joi.string().min(5).max(255).required().email(),
+		password: Joi.string().min(8).max(255).required(),
+	})
+	return schema.validate(req.body);
+}
+
+module.exports = router; 
